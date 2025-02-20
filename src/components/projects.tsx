7d932ba17ca99bc8e2c975efbb456ea7665e612c
Archie Habaradas <archiehabaradas3228@gmail.com>
2025-02-20 21:28:36 +0800

    added: new project item

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,62 +1,52 @@
 import Badge from "./badge";
 import Image from "next/image";
+import { ProjectsList } from "@/constants/projects";
+
+export type ProjectProps = {
+    id: number,
+    image?: string,
+    name: string,
+    description: string,
+    href: string,
+    tags?: string[],
+};
 
 const Projects = () => {
 
-    type ProjectProps = {
-        image: string,
-        name: string,
-        acronym?: string,
-        description: string,
-        tags?: string[]
-    };
-
-    const Project = ({image, name, acronym, description, tags} : ProjectProps) => (
-        <div className="flex items-start justify-between space-x-5 lg:p-5">
-            <Image src={image} alt="" width={100} height={50}/>
-            <div className="">
-                <h3 className="font-medium leading-snug text-slate-200">{name}</h3>
-                <p className="text-slate-500">{acronym}</p>
-                <p className="mt-2 text-sm leading-normal">{description}</p>
-                <ul className="flex flex-wrap mt-2">
-                    {tags?.map((item, i) => (
-                        <li key={i} className="mb-2 mr-2"><Badge tag={item}/></li>
-                    ))}
-                </ul>
+    const Project = ({image, name, description, href, tags} : ProjectProps) => (
+        <a href={href} target="_blank">
+            <div className="flex flex-col justify-center items-center lg:p-5">
+                <Image src={image ?? ''} alt="" layout="responsive" width={100} height={50}/>
+                <div className="mt-5">
+                    <h3 className="font-medium leading-snug text-dark-primary-txt">{name}</h3>
+                    <p className="mt-2 text-sm leading-normal text-dark-primary-txt/75">{description}</p>
+                    <ul className="flex flex-wrap mt-2">
+                        {tags?.map((item, i) => (
+                            <li key={i} className="mb-2 mr-2"><Badge tag={item}/></li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
+        </a>
     );
 
     return (
         <div className="section space-y-10" id="projects">
-            <Project 
-                image={'/images/image-placeholder.jpg'}
-                name={'Payroll Management System'}
-                acronym={'PMS'}
-                description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, tempora? At, 
-                recusandae quam! Architecto reiciendis nesciunt molestias quod dolores tenetur 
-                explicabo minus quasi aspernatur officia exercitationem, harum enim ipsam delectus.`}
-                tags={['VB.Net', 'MySQL']} 
-                />
-            <Project 
-                image={'/images/image-placeholder.jpg'}
-                name={'Data Management System'}
-                acronym={'DMS'}
-                description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, tempora? At, 
-                recusandae quam! Architecto reiciendis nesciunt molestias quod dolores tenetur 
-                explicabo minus quasi aspernatur officia exercitationem, harum enim ipsam delectus.`}
-                tags={['VB.Net', 'MySQL']} 
-                />
-            <Project 
-                image={'/images/image-placeholder.jpg'}
-                name={'Feedback System'}
-                description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, tempora? At, 
-                recusandae quam! Architecto reiciendis nesciunt molestias quod dolores tenetur 
-                explicabo minus quasi aspernatur officia exercitationem, harum enim ipsam delectus.`}
-                tags={['Android Studio', 'Java', 'SQLite']} 
-                />
+            {
+                ProjectsList.sort((a, b) => b.id - a.id).map((project: ProjectProps, i) => (
+                    <Project
+                        id={project.id}
+                        image={project.image}
+                        name={project.name}
+                        description={project.description}
+                        href={project.href}
+                        tags={project.tags}
+                        key={i}
+                    />
+                ))
+            }
         </div>
     );
 }
- 
+
 export default Projects;
---
+++ src/constants/projects.ts
@@ -0,0 +1,15 @@
+import { ProjectProps } from "@/components/projects";
+
+export const ProjectsList: ProjectProps[] = [
+    {
+        id: 1,
+        image: "/images/projects/lala-rent.png",
+        name: "Lala Rent",
+        description: `This web app is designed to provide a seamless limousine rental experience, 
+        allowing users to browse a fleet of luxury vehicles, select their preferred ride, and book effortlessly. 
+        Built with a sleek and user-friendly interface, the platform ensures a smooth reservation process, 
+        secure payments, and real-time availability updates.`,
+        href: "https://lala-store.infinityfreeapp.com/",
+        tags: ["Wordpress", "Elementor", "HTML", "CSS", "JavaScript", "PHP"],
+    }
+]
