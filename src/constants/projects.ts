ba1d0169ada60197abe25243b8f50d84f4ef2002
Archie Habaradas <archiehabaradas3228@gmail.com>
2025-02-20 21:56:32 +0800

    change: new project item description

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,11 +4,12 @@ export const ProjectsList: ProjectProps[] = [
     {
         id: 1,
         image: "/images/projects/lala-rent.png",
-        name: "Lala Rent",
-        description: `This web app is designed to provide a seamless limousine rental experience, 
-        allowing users to browse a fleet of luxury vehicles, select their preferred ride, and book effortlessly. 
-        Built with a sleek and user-friendly interface, the platform ensures a smooth reservation process, 
-        secure payments, and real-time availability updates.`,
+        name: "Lala Store Test Site",
+        description: `This is my test site for e-commerce web apps that I'm developing. 
+        It serves as a platform to experiment with features like product listings, user 
+        authentication, and seamless checkout experiences. One of my favorite projects 
+        is the 'Rent a Limousines' web app, which offers a sleek design, an intuitive 
+        booking system, and a responsive user interface for effortless limousine rentals.`,
         href: "https://lala-store.infinityfreeapp.com/",
         tags: ["Wordpress", "Elementor", "HTML", "CSS", "JavaScript", "PHP"],
     }
